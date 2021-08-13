92805b5881a0549d9a21d76e7da385dd0a5a1370
Мартынов Павел <Martynov@swecor.com>
2021-08-13 16:24:37 +0300

    Changes have been made to the task sortString

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,11 +4,16 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-    let newArr = arr.slice()
-    if (param === 'asc') {
-        return newArr.sort((a,b)=>a.localeCompare(b, ['ang-u-kf-upper', 'ru-u-kf-upper'], { sensitivity: 'variant' }))
-    } else if (param === 'desc') {
-        return newArr.sort((a,b)=>b.localeCompare(a, ['ang-u-kf-upper', 'ru-u-kf-upper'], { sensitivity: 'variant' }))
-    }
+export function sortStrings(arr, param = "asc") {
+  return arr
+    .slice()
+    .sort((a, b) =>
+      param === "asc"
+        ? a.localeCompare(b, ["ang-u-kf-upper", "ru-u-kf-upper"], {
+            sensitivity: "variant",
+          })
+        : b.localeCompare(a, ["ang-u-kf-upper", "ru-u-kf-upper"], {
+            sensitivity: "variant",
+          })
+    );
 }
