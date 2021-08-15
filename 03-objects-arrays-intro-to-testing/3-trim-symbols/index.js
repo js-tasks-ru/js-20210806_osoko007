e3f9549c25dc7f46b30274529721593004850025
Pavel Martynov <martynov.osoko@yandex.ru>
2021-08-15 21:17:52 +0300

    3th homework

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (size === 0) {
+    return "";
+  } else if (!string) {
+    return "";
+  } else if (string && size) {
+    let filterArray = [];
+    let newString = string.split("");
+    let count = 1;
+    newString.map((item, index, array) => {
+      if (array[index] === array[index + 1]) {
+        if (count < size) {
+          ++count;
+          filterArray.push(array[index]);
+        }
+      } else if (array[index] !== array[index + 1]) {
+        count = 0;
+        ++count;
+        filterArray.push(array[index]);
+      }
+    });
+    return filterArray.join("");
+  } else if (!size && string) {
+    return string;
+  }
 }
